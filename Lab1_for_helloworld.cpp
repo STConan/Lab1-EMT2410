52daebcf108bf8e4041f492de57abb403cc02ac9
STConan <109646115+STConan@users.noreply.github.com>
2022-09-30 01:45:48 -0400

    Update Lab1_for_helloworld.cpp

--- Lab1_for_helloworld.cpp
+++ Lab1_for_helloworld.cpp
@@ -9,19 +9,23 @@
 using namespace std;
 
 int i;
-int uinput;
+int user_input;
 
 int main()
 {
+    //Iteration to print Hello World! 5 times
     for (int i=0; i < 5; i++) 
     {
         cout << "Hello World!\n";
     }
     
+    //Printing rquest of user input
     cout << "Please input a number between 2-5\n";
-    
+    //Taking the user input
     cin >> uinput;
-
+    
+    //Iteration to use the user input and print Hello World for
+    //the ammount of times equal to the number the user inputted
     for (i = 0; i < uinput; i++)
     {
         cout << "Hello World!\n";
